0bafeacbdc813feb21c1dd8a42913771d70ced9b
Reed <Reed@DESKTOP-ARJ647B>
2024-07-23 15:40:09 -0400

    Add project files.

---
+++ RewstLibraryTest/Form1.Designer.vb
@@ -0,0 +1,99 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        TextBox1 = New TextBox()
+        TextBox2 = New TextBox()
+        Button1 = New Button()
+        RichTextBox1 = New RichTextBox()
+        Button2 = New Button()
+        SuspendLayout()
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Anchor = AnchorStyles.Top Or AnchorStyles.Left Or AnchorStyles.Right
+        TextBox1.Location = New Point(15, 14)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(773, 31)
+        TextBox1.TabIndex = 0
+        TextBox1.Text = "https://engine.rewst.io/webhooks/custom/trigger/0190e032-673e-7ac0-b3e8-2b44f2aa9b45/3f245a85-e037-40db-9845-4a61d0a8d0e2"
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Anchor = AnchorStyles.Top Or AnchorStyles.Left Or AnchorStyles.Right
+        TextBox2.Location = New Point(15, 51)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(773, 31)
+        TextBox2.TabIndex = 1
+        TextBox2.Text = "rewst-webhook-dispatchassistant"
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(15, 88)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(112, 34)
+        Button1.TabIndex = 2
+        Button1.Text = "Button1"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' RichTextBox1
+        ' 
+        RichTextBox1.Anchor = AnchorStyles.Top Or AnchorStyles.Bottom Or AnchorStyles.Left Or AnchorStyles.Right
+        RichTextBox1.Location = New Point(15, 128)
+        RichTextBox1.Name = "RichTextBox1"
+        RichTextBox1.Size = New Size(773, 310)
+        RichTextBox1.TabIndex = 3
+        RichTextBox1.Text = ""
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(133, 88)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(112, 34)
+        Button2.TabIndex = 4
+        Button2.Text = "Button2"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(10F, 25F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(Button2)
+        Controls.Add(RichTextBox1)
+        Controls.Add(Button1)
+        Controls.Add(TextBox2)
+        Controls.Add(TextBox1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents RichTextBox1 As RichTextBox
+    Friend WithEvents Button2 As Button
+
+End Class
---
+++ RewstLibraryTest/Form1.vb
@@ -0,0 +1,66 @@
+﻿Imports System.Net.Http
+Imports System.Net.Http.Json
+
+Public Class Form1
+
+    Const VAULT As String = "https://akv-2-isuztref6n4i4ee.vault.azure.net/"
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+
+    End Sub
+
+    Private Async Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Using hook As New Rewst.Webhook(TextBox1.Text, VAULT, TextBox2.Text)
+            Dim rulelist As RuleCollection = Await hook.CallAsync(Of RuleCollection)(HttpMethod.Get)
+            For Each r In rulelist.Rules
+                RichTextBox1.AppendText(r.Name)
+                RichTextBox1.AppendText(vbCrLf)
+            Next
+        End Using
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Using hook As New Rewst.Webhook(TextBox1.Text, VAULT, TextBox2.Text)
+            Dim d As New Doohicky
+            d.Name = "Doohicky"
+            d.Id = 123
+            d.Created = Now
+
+            Dim result = hook.Call(Of Doohicky, Hashtable)(HttpMethod.Post, d)
+            For Each key In result.Keys
+                RichTextBox1.AppendText($"{key}={result(key)}")
+                RichTextBox1.AppendText(vbCrLf)
+            Next
+
+        End Using
+    End Sub
+End Class
+
+Public Class Doohicky
+    Public Property Name As String
+    Public Property Id As Integer
+    Public Property Created As Date
+End Class
+
+Public Class RuleCollection
+    Public Property Rules As New List(Of Rule)
+End Class
+
+Public Class Rule
+    Public Property Name As String
+    Public Property Triggers As New List(Of ValueComparison)
+    Public Property Conditions As New List(Of ValueComparison)
+    Public Property Actions As New List(Of RuleAction)
+End Class
+
+Public Class ValueComparison
+    Public Source As String
+    Public Compare As String
+    Public Value As String
+End Class
+
+Public Class RuleAction
+    Public Type As String
+    Public Args As New List(Of String)
+End Class
